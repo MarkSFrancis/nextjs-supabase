354b94ab9c61588aaf93eef6b129120eb33a884b
MarkSFrancis <mrkfrancis8@gmail.com>
2021-11-14 20:23:19 +0000

    Improved navbar

--- components/Container/PageContainer.tsx
+++ components/Container/PageContainer.tsx
@@ -2,5 +2,5 @@ import { Container, ContainerProps, forwardRef } from '@chakra-ui/react';
 import React from 'react';
 
 export const PageContainer = forwardRef<ContainerProps, typeof Container>((props, ref) => (
-  <Container maxW="container.lg" py={50} {...props} ref={ref} />
+  <Container maxW="container.lg" py={6} {...props} ref={ref} />
 ));
--- views/SiteNavbar/SiteNavbar.tsx
+++ views/SiteNavbar/SiteNavbar.tsx
@@ -21,47 +21,49 @@ export const SiteNavbar: FC = () => {
   const session = useAuthSession();
 
   return (
-    <PageContainer px={0} py={2} mb={4}>
-      <Wrap justify="space-between" spacing={4}>
-        <NextLink href="/" passHref>
-          <IconButton as="a" icon={<>🏠</>} aria-label="Home page" />
-        </NextLink>
-        <Box flexGrow={1} maxW="lg">
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-            }}
-          >
-            <FormControl>
-              <VisuallyHidden>
-                <FormLabel>Search</FormLabel>
-              </VisuallyHidden>
-              <InputGroup>
-                <Input type="search" placeholder="Search" />
-                <InputRightAddon p={0} overflow="hidden">
-                  <IconButton
-                    borderRadius={0}
-                    type="submit"
-                    icon={<Search2Icon />}
-                    aria-label="Search"
-                  />
-                </InputRightAddon>
-              </InputGroup>
-            </FormControl>
-          </form>
-        </Box>
-        <HStack>
-          {session ? (
-            <NextLink href="/auth/my-profile" passHref>
-              <IconButton as="a" icon={<SettingsIcon />} aria-label="Settings" />
-            </NextLink>
-          ) : (
-            <Button as="a" href="#" onClick={(e) => e.preventDefault()}>
-              Sign in or register
-            </Button>
-          )}
-        </HStack>
-      </Wrap>
-    </PageContainer>
+    <Box boxShadow="sm">
+      <PageContainer px={0} py={2}>
+        <Wrap justify="space-between" spacing={4}>
+          <NextLink href="/" passHref>
+            <IconButton as="a" icon={<>🏠</>} aria-label="Home page" />
+          </NextLink>
+          <Box flexGrow={1} maxW="lg">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <FormControl>
+                <VisuallyHidden>
+                  <FormLabel>Search</FormLabel>
+                </VisuallyHidden>
+                <InputGroup>
+                  <Input type="search" placeholder="Search" />
+                  <InputRightAddon p={0} overflow="hidden">
+                    <IconButton
+                      borderRadius={0}
+                      type="submit"
+                      icon={<Search2Icon />}
+                      aria-label="Search"
+                    />
+                  </InputRightAddon>
+                </InputGroup>
+              </FormControl>
+            </form>
+          </Box>
+          <HStack>
+            {session ? (
+              <NextLink href="/auth/my-profile" passHref>
+                <IconButton as="a" icon={<SettingsIcon />} aria-label="Settings" />
+              </NextLink>
+            ) : (
+              <Button as="a" href="#" onClick={(e) => e.preventDefault()}>
+                Sign in or register
+              </Button>
+            )}
+          </HStack>
+        </Wrap>
+      </PageContainer>
+    </Box>
   );
 };
